0a838dfb071ddb79e89f67c34a2ad773883e8682
Stefan Stölzle <stoe@github.com>
2017-05-08 15:37:45 +0200

    Fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
   delay(1000);
-  turn the LED off by making the voltage LOW
+  // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
   delay(1000);
